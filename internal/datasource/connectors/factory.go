d1eae6a03397d0c556a88ccf33e95a6dc4fd3f68
Chidozie C. Okafor <39489124+doziestar@users.noreply.github.com>
2024-06-30 15:59:07 +0200

    refactor: Update data connector interface and implementation

--- internal/datasource/connectors/factory.go
+++ internal/datasource/connectors/factory.go
@@ -9,10 +9,10 @@ func ConnectorFactory(config *Config) (Connector, error) {
 	switch config.Type {
 	case "sql":
 		return NewSQLConnector(config), nil
-	case "nosql":
-		// return NewNoSQLConnector(config), nil
-	case "file":
-		// return NewFileConnector(config), nil
+	case "redis":
+		return NewRedisConnector(config), nil
+	case "mongo":
+		return NewMongoConnector(config), nil
 	case "api":
 		// return NewAPIConnector(config), nil
 	default:
