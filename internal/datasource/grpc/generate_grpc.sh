cecfe85c89544fa518bc27dde36063241ba3bd92
Chidozie C. Okafor <39489124+doziestar@users.noreply.github.com>
2024-07-01 22:50:21 +0200

    refactor: Generate gRPC client code for connector.proto

--- internal/datasource/grpc/generate_grpc.sh
+++ internal/datasource/grpc/generate_grpc.sh
@@ -6,6 +6,17 @@ PROTO_DIR="./internal/datasource/grpc/proto"
 # Set the output directory for generated Go files
 GO_OUT_DIR="./internal/datasource/grpc"
 
+CLIENT_OUT_DIR="./internal/datasource/grpc/client"
+
+# Ensure the output directory exists
+mkdir -p $CLIENT_OUT_DIR
+
+# Generate Go code for client
+protoc --proto_path=${PROTO_DIR} \
+       --go_out=${CLIENT_OUT_DIR} --go_opt=paths=source_relative \
+       --go-grpc_out=${CLIENT_OUT_DIR} --go-grpc_opt=paths=source_relative \
+       ${PROTO_DIR}/connector.proto
+
 # Generate Go code
 protoc --proto_path=${PROTO_DIR} \
        --go_out=${GO_OUT_DIR} --go_opt=paths=source_relative \
